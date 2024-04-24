44896badc2805985304857d2fd6a3529d6bf2eb9
jiyun <ljy3852@naver.com>
2024-04-24 21:24:50 +0900

    main화면단 추가,수정 이동버튼 완료 누르면 이동하게 끔 완료 추가, 이지윤 수정, 김지유

---
+++ lib/insert.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Insert extends StatelessWidget {
+  const Insert({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: true,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/update.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class Update extends StatelessWidget {
+  const Update({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(),
+        ),
+      ),
+    );
+  }
+}
