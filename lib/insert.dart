c165bd1a46ad21c9ce074c824f96d3ffdd83b412
jiyun <ljy3852@naver.com>
2024-04-25 15:44:23 +0900

    main화면단 추가 화면단 완성 title content writer 3개

--- lib/insert.dart
+++ lib/insert.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trio/main.dart';
 
 class Insert extends StatelessWidget {
   const Insert({super.key});
@@ -10,7 +11,78 @@ class Insert extends StatelessWidget {
         top: true,
         child: SingleChildScrollView(
           child: Column(
-            children: [],
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Text("Board Register",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              Container(
+                child: Text("Tilte",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'title',
+                  ),
+                ),
+              ),
+              Container(
+                child: Text("Text area",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Text area',
+                  ),
+                ),
+              ),
+              Container(
+                child: Text("Writer",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Writer',
+                  ),
+                ),
+              ),
+
+              InkWell(
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyApp()),
+                  );
+                },
+                child: Container(
+                  color: Colors.amber,
+                  child: Text(
+                    "button"
+                  ),
+                ),
+              ),
+
+            ],
           ),
         ),
       ),
