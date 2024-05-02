640e4546a16961f25b4fe811b751601d0ed4beb3
charles <rmswo417@naver.com>
2024-05-02 16:36:16 +0900

    잘해라

---
+++ lib/screen/join_page.dart
@@ -0,0 +1,15 @@
+//
+//
+//
+// import 'package:flutter/material.dart';
+//
+// class JoinPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Navigator.of(context),
+//     )
+//   }
+//
+//
+// }
